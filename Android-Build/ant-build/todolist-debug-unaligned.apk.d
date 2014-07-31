db7a34866332a765d745af574ed663a3f982c6e3
Andrew Reynolds <andrew.reynolds@gtnexus.com>
2014-07-31 14:14:16 -0400

    Initial Android Build

---
+++ Android-Build/ant-build/todolist-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\areynolds\Documents\Todo1.0\phonegap\platforms\android\ant-build\todolist-debug-unaligned.apk : \
+C:\Users\areynolds\Documents\Todo1.0\phonegap\platforms\android\ant-build\todolist.ap_ \
+C:\Users\areynolds\Documents\Todo1.0\phonegap\platforms\android\ant-build\classes.dex \
+C:\Users\areynolds\Documents\Todo1.0\phonegap\platforms\android\CordovaLib\ant-build\classes.jar \
